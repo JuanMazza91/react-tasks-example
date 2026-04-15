d4fd8d7ba3b0147b1e4cf6c7c351d9ab8136acf8
JuanMazza91 <juance_t@hotmail.com>
2026-04-15 14:47:02 -0300

    firt commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "Prime tareas",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "Segunda tareas",
+  },
+  {
+    id: 2,
+    title: "mi tercer tarea",
+    description: "Tercer tareas",
+  },
+];
